378dcaa9bd2ddc3b8662e367191e537a55cc1668
Igor Todorovski <itodorov@ca.ibm.com>
2025-08-27 10:08:58 -0400

    Initial commit

---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/alternativesport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/alternativesport.git'), string(name: 'PORT_DESCRIPTION', value: 'A system tool for maintaining the /etc/rc*.d hierarchy' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+  }
+}
